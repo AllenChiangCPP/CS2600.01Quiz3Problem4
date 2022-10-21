cf06c9c1d12eef9b7d2f721e1f881b6e409f0265
Allen <allenchiang@cpp.edu>
2022-10-21 16:52:44 -0700

    created magicSquare file and added outline

---
+++ magicSquare.c
@@ -0,0 +1,10 @@
+//magic square checker, takes a 2d array
+//check if sum of all entries is 45 to check for no repeats
+//check sum of rows and columns to be 15 for all rows and colunmns
+//return 1 if is magic square, return 0 if not
+//int magicSquareCheck(int givenSquare[3][3])
+
+//int main()
+    //create 2d array that has a Lo Shu Magic Square
+    //create 2d array that fails the Lo Shu Magic Square Test
+
