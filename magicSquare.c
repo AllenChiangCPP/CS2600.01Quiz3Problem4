6a67965318829f93d11ccb7fef0b095b41844d6e
Allen <allenchiang@cpp.edu>
2022-10-21 21:11:55 -0700

    added code for randomly generating a square as randomizeSquare and added function squarePrinter for printing a square

--- magicSquare.c
+++ magicSquare.c
@@ -1,4 +1,7 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include <unistd.h>
 //magic square checker, takes a 2d array
 
 //return 1 if is magic square, return 0 if not
@@ -21,22 +24,83 @@ int magicSquareCheck(int givenSquare[3][3])
     }
 }
 //add squarePrinter to print out 2d array as a square for later use
-//add generateSquare function
-    //add function that generates a random 2-d array with random numbers 1-9
-    //shuffle a 2d array randomly that contains 1-9
+void squarePrinter(int givenSquare[3][3])
+{
+    printf("+---+---+---+\n");
+    printf("| %d | %d | %d |\n", givenSquare[0][0], givenSquare[0][1], givenSquare[0][2]);
+    printf("+-----------+\n");
+    printf("| %d | %d | %d |\n", givenSquare[1][0], givenSquare[1][1], givenSquare[1][2]);
+    printf("+-----------+\n");
+    printf("| %d | %d | %d |\n", givenSquare[2][0], givenSquare[2][1], givenSquare[2][2]);
+    printf("+-----------+\n");
+}
+//add randomizeSquare function
+//add function that generates a random 2-d array with random numbers 1-9
+int** randomizeSquare(int square[3][3])
+{   
+    int randomRow = 0;
+    int randomColumn = 0;
+    int temp;
+    //shuffle a given 2d array randomly 
+    int value = 1;
+    for(int i = 0; i < 3; i ++)
+    {
+        for(int j = 0; j < 3; j++)
+        {
+            randomRow = rand() % 3;
+            randomColumn = rand() % 3;
+            temp = square[i][j];
+            square[i][j] = square[randomRow][randomColumn];
+            square[randomRow][randomColumn] = temp;
+        }
+    }
+}
+
 int main()
 {
+    srand(time(NULL) * getpid()); //for random generation in randomizeSquare
     //create 2d array that has a Lo Shu Magic Square
     int loShu[3][3] = {{4,9,2},{3,5,7},{8,1,6}};
-    printf("loShu success test: %d\n",magicSquareCheck(loShu));
+    printf("Magic square success test: \n");
+    squarePrinter(loShu);
+    if(magicSquareCheck(loShu) == 1)
+    {
+        printf("successful square \n");
+    }
+    else
+    {
+        printf("unsuccessful square \n");
+    }
+    printf("\n");
     //create 2d array that fails the Lo Shu Magic Square 
     int loShuFail[3][3] = {{2,4,9},{3,5,7},{6,8,1}};
-    printf("loShu failure test: %d\n",magicSquareCheck(loShuFail));
-    return 0;
-
-    //add while loop, generateSquare function until generate a Lo Shu square
+    printf("Magic square failure test: \n");
+    squarePrinter(loShuFail);
+    if(magicSquareCheck(loShuFail) == 1)
+    {
+        printf("successful square \n");
+    }
+    else
+    {
+        printf("unsuccessful square \n");
+    }
+    printf("\n");
+    printf("Square with random numbers test.\n");
     //add counter, starts at 0, goes up for each failed magic square
+    int magicSquareAttempts = 0;
+    int defaultSquare[3][3] = {{1,2,3},{4,5,6},{7,8,9}};
+    randomizeSquare(defaultSquare);
+    //add while loop, randomizeSquare function until generate a Lo Shu square
+    while(magicSquareCheck(defaultSquare) == 0)
+    {
+        randomizeSquare(defaultSquare);
+        magicSquareAttempts++;
+    }
     //print square using squarePrinter
+    printf("Number of squares generated before success: %d\n", magicSquareAttempts);
+    printf("Successful square generated: \n");
+    squarePrinter(defaultSquare);
+    return 0;
 }
     
 
